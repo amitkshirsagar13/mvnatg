8d2ba23ac4e60bb202964503cfc1232c9886c9a5
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2017-10-06 14:10:36 +0530

    Working Setup

---
+++ src/mvnatg/common/src/atg/nucleus/logging/Slf4jLogger.java
@@ -0,0 +1,25 @@
+package atg.nucleus.logging;
+
+import atg.nucleus.GenericService;
+
+/**
+ * 
+ * <p>
+ * <b>Overview:</b>
+ * <p>
+ * 
+ * 
+ * <pre>
+ * &#64;projectName AtgBaseModule
+ * Creation date: Apr 27, 2017
+ * &#64;author Amit Kshirsagar
+ * &#64;version 1.0
+ * &#64;since
+ * 
+ * <p><b>Modification History:</b><p>
+ * 
+ * 
+ * </pre>
+ */
+public class Slf4jLogger extends GenericService {
+}
